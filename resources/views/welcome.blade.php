0277e8241cece67aa02c758a7f77f642a271f3b9
LamiaaAssy <lamiaa.assy@gmail.com>
2019-04-25 13:44:37 +0200

    welcome footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,486 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE HTML>
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+    
+    <title >Courses Camp</title>
+    <style type="text/css">
+        html {
+    background-color: white ;
+    color: #555 ;
+    font-family: 'lato','arial', sans-serif;
+    font-size: 20px ;
+    text-rendering: optimizeLegibility;
+    font-weight: 300; 
+    overflow-x:hidden ; 
+}    
+        header{
+            background-image:linear-gradient(rgba(0, 0, 0, 0.35) , rgba(0, 0, 0, 0.35)),url(webhome/vendors/css/img/black-and-white-man-person-hands.jpg);
+            background-position: center ; 
+            background-size: cover ;
+            height: 100vh ;
+            background-attachment: fixed ; 
+            
+        }
+        #logo {
+            margin-top: 20px ;
+            margin-left: -20px ; 
+            height: 100px ; 
+            width: auto ; 
+            
+        }
+        h1  {
+            position: fixed; 
+            left: 30% ;
+            top:-70px ; 
+            font-weight: 400 ; 
+            font-style: bold ;
+            font-size: 400%; 
+            
+        }
+        #first-p{
+            color: white;
+            padding-top: 25px ;
+            position: relative;
+            left: 40%;
+            font-size: 120% ;
+        }
+        #fcih {
+            padding-top: 5px ;
+            position: fixed ; 
+            left: 46%;
+            color: #E67E22 ;
+            font-size: 150%;
+            font-weight: 700 ;
+        }
+        .capital {
+            color: #E67E22; 
+            font-weight: 700 ;
+        }
+        #first-btm {
+            padding-top: 60px ;
+            position: fixed  ;
+            left: 36% ;
+        }
+        .courses {
+             margin: 0 ! important ; 
+            
+        }
+        .courses-info {
+            margin: 40px ;
+            padding: 15px ; 
+            background-color: #ebebeb; 
+            height: 330px;
+        }
+        h2 {
+            color: black ; 
+        }
+        #course-title {
+            font-size: 200% ;
+            
+        }
+        .ion-person {
+            color: #e67e22 ; 
+            font-size: 30px ; 
+            padding: 5px 5px 5px 20px ;
+        }
+        
+        .instructor {
+            font-size: 20px ;
+            padding-left: 10px;
+        }
+        .description {
+            width: 35%;
+            padding: 5px 5px 5px 50px ;
+            color: #8d8d8d;
+            font-size: 80%; 
+
+        }
+        .content {
+            width: 15%;
+            font-size: 15px ;
+            position: relative;
+            left: 38%;
+            bottom: 110px ; 
+
+        }
+        a:link,
+        a:visited {
+            color:cornflowerblue ; 
+            text-decoration: none ; 
+            padding-bottom: 1px ; 
+            border-bottom: 1px solid cornflowerblue ; 
+            transition: border-bottom .2s,  color 1s ;
+        }
+
+        a:hover, 
+        a:active {
+            color: #E67E22  ; 
+            border-bottom: 1px solid transparent ; 
+
+        }
+
+        .video {
+            position:relative ; 
+            left: 60%;
+            bottom: 275px ; 
+            width: 450px ;
+            height: 195px;
+             
+            
+        }
+        /*=======external====*/
+        /*===================*/
+        /*===================*/
+        
+        /*=====Headder========*/
+         .hero-text-box {
+                position: absolute ; 
+                width: 1140px ; 
+                left : 50% ; 
+                top : 50% ;
+                transform: translate(-50% , -50%) ;
+
             }
+        .main-nav {
+            float: right ; 
+            list-style: none ; 
+            margin-top: 40px ;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .main-nav li {
+            display: inline-block ; 
+            margin-left: 40px ; 
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        }
+        .main-nav li a:link, 
+        .main-nav li a:visited {
+            padding: 8px 0px ; 
+            color: white ; 
+            text-decoration: none ; 
+            text-transform: uppercase ; 
+            font-size: 90% ; 
+            border-bottom: 2px solid transparent ;
+            transition:  border-bottom .02s; 
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        }
+        .main-nav li a:hover, 
+        .main-nav li a:active {
+            border-bottom: 2px solid #e67e22 ; 
+        }
+        /*=====Headding========*/
+        h1,
+        h2 {
 
-            .content {
-                text-align: center;
-            }
+            font-weight: 300 ; 
+            text-transform: uppercase ; 
+            letter-spacing: .5px ; 
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        h1 {
+        
+            color: white ; 
+            word-spacing: 3px ; 
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
+        h2 {
+
+            font-size: 180% ;
+            word-spacing: 2px;
+            text-align: center;
+            margin-bottom: 30px ;
+
+    }
+    h2:after {
+        display: block;
+        height: 2px ;
+        width: 100px ;
+        background-color:#e67e22;
+        content: " ";
+        margin: 0 auto ;
+        margin-top: 30px ;
+    }
+
+    h3{
+        margin-bottom: 15px;
+    }
+        /*-------------------------------*/ 
+        /*--------SETUP------------------*/
+        /*-------------------------------*/ 
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        html {
+            background-color: white ;
+            color: #555 ;
+            font-family: 'lato','arial', sans-serif;
+            font-size: 20px ;
+            text-rendering: optimizeLegibility;
+            font-weight: 300; 
+        }
+
+        
+        /*-------------------------------*/ 
+        /*--------REUSNABLE------------------*/
+        /*-------------------------------*/
+
+        section {
+            padding: 80px 0px ;
+        }
+
+        .row {
+            max-width: 1140px;
+            margin: 0 auto;
+        }
+
+        
+
+        /*-------------------*/
+        /*-----botton--------*/
+        /*-------------------*/
+        .btm:link,
+        .btm:visited,
+        input[type=submit] {
+             display: inline-block ; 
+             padding: 15px 30px ; 
+             font-weight: 300px ; 
+             text-decoration: none ; 
+             border-radius: 200px ; 
+             transition: background-color .2s , color .2s , border .2s ; 
+
+        }
+
+        .btm-full:link,
+        .btm-full:visited,
+        input[type=submit] {
+            background-color: #e67e22; 
+            color: white; 
+            border: 1px solid #e67e22 ; 
+            margin-right: 15px ; 
+
+        }
+
+        .btm-ghost:link ,
+        .btm-ghost:visited {
+            border: 1px solid #e67e22 ; 
+            color: #e67e22 ; 
+
+        }
+
+        .btm:hover,
+        .btm:active,
+        input[type=submit]:hover,
+        input[type=submit]:active {
+
+            background-color: #c97122 ;
+
+        }
+
+        .btm-full:hover,
+        .btm-full:active,
+        input[type=submit] {
+            border: 1px solid #e67e22 ; 
+
+        }
+
+        .btm-ghost:hover ,
+        .btm-ghost:active {
+            border: 1px solid #e67e22 ; 
+            color: #fff ; 
+        }
+
+    </style>
+
+</head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+        <header> 
+            <nav>
+                <div class="row">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <img src="{{ asset('webhome') }}/vendors/css/img/logo3.png" id="logo" >
+                    <ul class="main-nav">
+                        @if (Route::has('login'))
+                        <div class="top-right links">
+                        @if (Auth::check())
+                        <li> <a href="#">About</a></li>
+                        <li> <a href="#">contact</a></li>
+                    <?php
+                        $user = Auth::user();
+                        // var_dump($user->admin);
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                        if($user->admin){
+                    ?>
+                        <li> <a href="{{ url('/dashboard') }}">Home</a></li>
+                        <?php
+                        } 
+                        else{
+                        ?>
+                            <li> <a href="{{ url('/home') }}">Home</a></li>
+                        <?php 
+                        }
+                        ?>
+                        <li> <a href="{{ route('logout') }}"onclick="event.preventDefault(); 
+                        document.getElementById('logout-form').submit();">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                     </form>
+                        </li>
+                        @else
+                        <li> <a href="#">About</a></li>
+                        <li> <a href="#">contact</a></li>
+                        <li> <a href="{{ url('/register') }}">Registration</a></li>
+                        <li> <a href="{{ url('/login') }}">Login</a></li>
+                        @endif
+                        </div>
+                        @endif
+
+                    </ul>
                 </div>
+                <!--
+                  <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    Logout
+                     </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                     </form>
+                -->
+            </nav>
+                <div class="hero-text-box">
+                    <h1>Write Now</h1>
+                    <p id="first-p"><span class="capital">F</span>ree <span class="capital">C</span>ources <span class="capital">i</span>n <span class="capital">H</span>ome</p>
+                    <p id="fcih">FCIH</p>
+                    <div id="first-btm">
+                    <a class="btm btm-full" href="#">Premium</a>
+                    <a class="btm btm-ghost" href="#">Totally Free</a>
+                    </div>
+                </div>
+        </header>
+        <section class="courses">
+            <h2>Choose To Start</h2>
+            <div class="courses-info" >
+                <p id="course-title">Complete Web Developer 2.0</p>
+                <i class="ion-person"><span class="instructor">Rob Percival , Web Developer And Teacher .</span></i>
+                <p class="description">You’re here because you’re ready to start building professional, career-boosting websites.
+
+Or maybe you’re coding already and want to take things to the next level?
+
+I’m here because I’m the creator of the best-selling, highest-rated coding courses on Udemy and I love to share my secrets with you.
+
+</p>
+                <ul class="content">
+                    <li>Lectures: 304</li>
+                    <li>Video: 30.5 hours</li>   
+                    <li>Languages: English</li>
+                    <li>Skill level: Beginner Level</li>
+                    <li><a href="https://www.udemy.com/the-complete-web-developer-course-2">Start Now</a></li>
+                </ul>
+        
+                <iframe class="video" src="https://www.youtube.com/embed/IsDwQeEgyi0" frameborder="10" allowfullscreen></iframe>
+
+
             </div>
-        </div>
+            
+            <div class="courses-info" >
+                <p id="course-title">Complete Web Developer 2.0</p>
+                <i class="ion-person"><span class="instructor">Rob Percival , Web Developer And Teacher .</span></i>
+                <p class="description">You’re here because you’re ready to start building professional, career-boosting websites.
+
+Or maybe you’re coding already and want to take things to the next level?
+
+I’m here because I’m the creator of the best-selling, highest-rated coding courses on Udemy and I love to share my secrets with you.
+
+</p>
+                <ul class="content">
+                    <li>Lectures: 304</li>
+                    <li>Video: 30.5 hours</li>   
+                    <li>Languages: English</li>
+                    <li>Skill level: Beginner Level</li>
+                    <li><a href="https://www.udemy.com/the-complete-web-developer-course-2">Start Now</a></li>
+                </ul>
+        
+                <iframe class="video" width="" src="https://www.youtube.com/embed/IsDwQeEgyi0" frameborder="10" allowfullscreen></iframe>
+
+
+            </div>
+            
+            
+            
+            
+
+        </section>
+              <footer class="footer-distributed">
+
+            <div class="footer-left">
+
+                <h3>Courses<span>Camp</span></h3>
+
+                <p class="footer-links">
+                    <a href="admin home">Home</a>
+                    
+                    <a href="homecontent">Log out</a>                 
+                  
+                </p>
+
+                <p class="footer-company-name">FCIH &copy; 2019</p>
+            </div>
+
+            <div class="footer-center">
+
+                <div>
+                    <i class="fa fa-map-marker"></i>
+                    <p><span>Helwan University</span> cairo, Egypt</p>
+                </div>
+
+                <div>
+                    <i class="fa fa-phone"></i>
+                    <p>+20 122 123456</p>
+                </div>
+
+                <div>
+                    <i class="fa fa-envelope"></i>
+                    <p><a href="mailto:support@fcih.com.com">support@fcih.com</a></p>
+                </div>
+
+            </div>
+
+            <div class="footer-right">
+
+                <p class="footer-company-about">
+                    <span>About the company</span>
+ We are the right place that will provide you courses that will help you in your life.          
+                </p>
+
+                <div class="footer-icons">
+
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-github"></i></a>
+
+                </div>
+
+            </div>
+
+        </footer>
+
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('webhome.homecontent.homecontent');
+    return view('welcome');
 });
 
 
