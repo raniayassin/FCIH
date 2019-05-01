77d7250f5b4a9c85b115c44a183580d8da4906f5
rania yassin <rania.yassin96@gmail.com>
2019-05-01 11:31:16 +0200

    add header $ footer to courses.blade

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,391 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+        <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+
+    <title >Cources Camp</title>
+    <style type="text/css">
+
+        .app-screen {
+            position: relative;
+            float: left ; 
+            left: 20% ; 
+            width: 20% ; 
+        }
+        .works-step 
+        ,.bttm-apps{
+            position: relative ; 
+            left: 27%; 
+        }
+        
+            html {
+    background-color: white ;
+    color: #555 ;
+    font-family: 'lato','arial', sans-serif;
+    font-size: 20px ;
+    text-rendering: optimizeLe gibility;
+    font-weight: 300; 
+    overflow-x:hidden ; 
+}    
+        header{
+            position: center ; 
+            background-size: cover ;
+            height: 15vh ;
+            background-attachment: fixed ; 
+            
+        }
+        #logo {
+            margin-top: 20px ;
+            margin-left: -20px ; 
+            height: 60px ; 
+            width: auto ; 
+            
+        }
+        h1  {
+            position: fixed; 
+            left: 30% ;
+            top:-70px ; 
+            font-weight: 400 ; 
+            font-style: bold ;
+            font-size: 400%; 
+            
+        }
+        #first-p{
+            color: white;
+            padding-top: 25px ;
+            position: relative;
+            left: 40%;
+            font-size: 120% ;
+        }
+        #fcih {
+            padding-top: 5px ;
+            position: fixed ; 
+            left: 46%;
+            color: #E67E22 ;
+            font-size: 150%;
+            font-weight: 700 ;
+        }
+        .capital {
+            color: #E67E22; 
+            font-weight: 700 ;
+        }
+        #first-btm {
+            padding-top: 60px ;
+            position: fixed  ;
+            left: 36% ;
+        }
+        .courses {
+             margin: 0 ! important ; 
+            
+        }
+        .courses-info {
+            margin: 30px ;
+            padding: 15px ; 
+            background-color: white; 
+            height: 55px;
+        }
+        h2 {
+            color: black ; 
+        }
+
+
+        .content {
+            width: 15%;
+            font-size: 15px ;
+            position: relative;
+            left: 38%;
+            bottom: 110px ; 
+            padding-right: 40px;
+
+        }
+        a:link,
+        a:visited {
+            color:cornflowerblue ; 
+            text-decoration: none ; 
+            padding-bottom: 1px ; 
+            border-bottom: 1px solid cornflowerblue ; 
+            transition: border-bottom .2s,  color 1s ;
+        }
+
+        a:hover, 
+        a:active {
+            color: #E67E22  ; 
+            border-bottom: 1px solid transparent ; 
+
+        }
+
+
+             
+            
+        }
+        /*=======external====*/
+        /*===================*/
+        /*===================*/
+        
+        /*=====Headder========*/
+         .hero-text-box {
+                position: absolute ; 
+                width: 1140px ; 
+                left : 50% ; 
+                top : 6% ;
+                transform: translate(-50% , -50%) ;
+
+            }
+        .main-nav {
+            float: right ; 
+            list-style: none ; 
+            margin-top: 40px ;
+        }
+
+        .main-nav li {
+            display: inline-block ; 
+            margin-left: 40px ; 
+
+        }
+        .main-nav li a:link, 
+        .main-nav li a:visited {
+            padding: 8px 0px ; 
+            color: white ; 
+            text-decoration: none ; 
+            text-transform: uppercase ; 
+            font-size: 85% ; 
+            border-bottom: 2px solid transparent ;
+            transition:  border-bottom .02s; 
+
+
+        }
+        .main-nav li a:hover, 
+        .main-nav li a:active {
+            border-bottom: 2px solid #e67e22 ; 
+        }
+        /*=====Headding========*/
+        h1,
+        h2 {
+
+            font-weight: 300 ; 
+            text-transform: uppercase ; 
+            letter-spacing: .5px ; 
+        }
+
+        h1 {
+        
+            color: white ; 
+            word-spacing: 3px ; 
+            left : 30%;
+            top :10%;
+    }
+
+        h2 {
+
+            font-size: 180% ;
+            word-spacing: 2px;
+            text-align: center;
+            margin-bottom: 30px ;
+
+    }
+    h2:after {
+        display: block;
+        height: 2px ;
+        width: 100px ;
+        background-color:#e67e22;
+        content: " ";
+        margin: 0 auto ;
+        margin-top: 30px ;
+    }
+
+    h3{
+        margin-bottom: 15px;
+    }
+        /*-------------------------------*/ 
+        /*--------SETUP------------------*/
+        /*-------------------------------*/ 
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        html {
+            background-color: white ;
+            color: #555 ;
+            font-family: 'lato','arial', sans-serif;
+            font-size: 20px ;
+            text-rendering: optimizeLegibility;
+            font-weight: 300; 
+        }
+
+        
+        /*-------------------------------*/ 
+        /*--------REUSNABLE------------------*/
+        /*-------------------------------*/
+
+        section {
+            padding: 80px 0px ;
+        }
+
+        .row {
+            max-width: 1140px;
+            margin: 0 auto;
+        }
+
+        
+
+        /*-------------------*/
+        /*-----botton--------*/
+        /*-------------------*/
+        .btm:link,
+        .btm:visited,
+        input[type=submit] {
+             display: inline-block ; 
+             padding: 15px 250px ; 
+             margin: 0 0 0 330px;
+             font-weight: 300px ; 
+             text-decoration: none ; 
+             border-radius: 300px ; 
+             transition: background-color .2s , color .2s , border .2s ; 
+
+        }
+
+        .more {
+            padding: 0 0 0 15px;
+        }
+
+
+        .btm-full:link,
+        .btm-full:visited,
+        input[type=submit] {
+            background-color: #e67e22; 
+            color: white; 
+            border: 1px solid #e67e22 ; 
+            margin-right: 15px 0px 50px 0px ; 
+
+        }
+
+        .btm-ghost:link ,
+        .btm-ghost:visited {
+            border: 1px solid #e67e22 ; 
+            color: #e67e22 ; 
+
+        }
+
+        .btm:hover,
+        .btm:active,
+        input[type=submit]:hover,
+        input[type=submit]:active {
+
+            background-color: #c97122 ;
+
+        }
+
+        .btm-full:hover,
+        .btm-full:active,
+        input[type=submit] {
+            border: 1px solid #e67e22 ; 
+
+        }
+
+        .btm-ghost:hover ,
+        .btm-ghost:active {
+            border: 1px solid #e67e22 ; 
+            color: #fff ; 
+        }
+
+    </style>
+
+
+</head>
+
+<body>
+            <header> 
+            <nav>
+                <div class="row">
+                    <img src="{{ asset('webhome') }}/vendors/css/img/logo3.png" id="logo" >
+                    <ul class="main-nav">
+                         @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        <li> <a href="{{ url('/dashboard') }}">Home</a></li>
+                        <li> <a href="{{ route('logout') }}"onclick="event.preventDefault();  document.getElementById('logout-form').submit();">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" >
+                    {{ csrf_field() }}
+                     </form>
+                        </li>
+                        
+                    </ul>
+                    
+                </div>
+            </nav>
+
+        </header>
+
+        <section>
+            
+        </section>
+
+                        <footer class="footer-distributed">
+
+            <div class="footer-left">
+
+                <h3>Courses<span>Camp</span></h3>
+
+                <p class="footer-links">
+                    <a href="{{ url('/dashboard') }}">Home</a>
+                    
+                    <a href="homecontent">Log out</a>                 
+                  
+                </p>
+
+                <p class="footer-company-name">FCIH &copy; 2019</p>
+            </div>
+
+            <div class="footer-center">
+
+                <div>
+                    <i class="fa fa-map-marker"></i>
+                    <p><span>Helwan University</span> cairo, Egypt</p>
+                </div>
+
+                <div>
+                    <i class="fa fa-phone"></i>
+                    <p>+20 122 123456</p>
+                </div>
+
+                <div>
+                    <i class="fa fa-envelope"></i>
+                    <p><a href="mailto:support@fcih.com.com">support@fcih.com</a></p>
+                </div>
+
+            </div>
+
+            <div class="footer-right">
+
+                <p class="footer-company-about">
+                    <span>About the company</span>
+ We are the right place that will provide you courses that will help you in your life.          
+                </p>
+
+                <div class="footer-icons">
+
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-github"></i></a>
+
+                </div>
+
+            </div>
+
+        </footer>
+
+</body>
+</html>
