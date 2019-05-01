145b621c26fd724490ca97b9d72a033283c22be8
Aishasamir <aishasameer423@gamil.com>
2019-05-01 17:23:26 +0200

    edit users dashboard

--- resources/views/usersboard.blade.php
+++ resources/views/usersboard.blade.php
@@ -313,7 +313,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        <li> <a href="{{ url('/') }}">Home</a></li>
+                        <li> <a href="{{ url('/dashboard') }}">Home</a></li>
                         <li> <a href="{{ route('logout') }}"onclick="event.preventDefault();  document.getElementById('logout-form').submit();">Logout</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                     {{ csrf_field() }}
@@ -360,7 +360,7 @@
                 <h3>Courses<span>Camp</span></h3>
 
                 <p class="footer-links">
-                    <a href="admin home">Home</a>
+                    <a href="{{ url('/dashboard') }}">Home</a>
                     
                     <a href="homecontent">Log out</a>                 
                   
